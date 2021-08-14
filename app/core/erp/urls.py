9949d58ce191c2a672970366ec6272f71930230e
jframac <jframac@gmail.com>
2021-08-14 00:28:21 -0500

    final video 20

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.erp.views.category.views import category_list
+from core.erp.views.category.views import CategoryListView
 
 app_name = 'erp'
 
 urlpatterns = [
-    path('category/list/', category_list, name='category_list'),
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
 ]
--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from core.erp.models import Category
+
 
 def category_list(request):
     data = {
@@ -7,3 +10,13 @@ def category_list(request):
         'categories': Category.objects.all(),
     }
     return render(request, 'category/list.html', data)
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = "category/list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorías'
+        return context
